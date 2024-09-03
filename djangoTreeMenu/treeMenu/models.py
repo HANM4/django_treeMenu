771b9091d842c6a9d4e47119a06e6f42484b4681
HANM4 <gfy-gfy@mail.ru>
2024-09-04 00:57:45 +0500

    feat:add models for treeMenu - add model TreeMenu - add BranchMenu

--- djangoTreeMenu/treeMenu/models.py
+++ djangoTreeMenu/treeMenu/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class TreeMenu(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+
+
+class BranchMenu(models.Model):
+    name = models.CharField(max_length=100)
+    menu = models.ForeignKey(TreeMenu, on_delete=models.CASCADE)
+    parent = models.ForeignKey("self",
+                               on_delete=models.CASCADE,
+                               related_name="children",
+                               null=True,
+                               blank=True)
+    slug = models.SlugField(unique=True, blank=True, db_index=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slag = self.generate_unique_slug(self.name)
+        super().save(*args, **kwargs)
+
+    def generate_unique_slug(self, name):
+        slug = slugify(name)
+        unique_slug = slug
+        num = 1
+        while BranchMenu.objects.filter(slug=unique_slug).exists():
+            unique_slug = '{}-{}'.format(slug, num)
+            num += 1
+        return unique_slug
+
